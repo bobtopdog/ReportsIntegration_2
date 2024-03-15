f07396f693de5b2a1032037a5de6b7c034086c0c
topdog <topdog@DESKTOP-SPS7F6P>
2024-03-15 11:28:42 +0000

    Initial

---
+++ ReportsIntegration/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReportsIntegration._Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <style>
+        html,body,form,#div1 {
+            height: 100%; 
+        }
+    </style>
+    <rsweb:ReportViewer ID="ReportViewer1" runat="server" ProcessingMode="Remote"  Width="100%" ShowToolBar="False">
+        <ServerReport ReportServerUrl="http://desktop-sps7f6p/reportserverSSRS" ReportPath="/incidents" />
+    </rsweb:ReportViewer>
+
+    <rsweb:ReportViewer ID="ReportViewer2" runat="server"  Width="100%" ShowToolBar="False"></rsweb:ReportViewer>
+</asp:Content>
---
+++ ReportsIntegration/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReportsIntegration
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ReportViewer2.ProcessingMode = ProcessingMode.Local;
+
+                LocalReport localReport = ReportViewer2.LocalReport;
+
+                localReport.ReportPath = "Reports/incidents.rdlc";
+
+                DataSet dataset = new DataSet("DataSet1");
+                string filterYear = "2024";
+
+                GetReportData(filterYear, ref dataset);
+
+                ReportDataSource dsSalesOrder = new ReportDataSource();
+                dsSalesOrder.Name = "DataSet1";
+                dsSalesOrder.Value = dataset.Tables["DataSet1"];
+
+                localReport.DataSources.Add(dsSalesOrder);
+
+
+            }
+        }
+
+        private void GetReportData(string filterYear, ref DataSet dsSalesOrder)
+        {
+            string sqlSalesOrder = "SELECT   tbl_test_chart_3.rowID   ,tbl_test_chart_3.typeOfIncident ,DateName(month,[dateSubmitted]) as [dateSubmitted]   ,tbl_test_chart_3.rnid ,month([dateSubmitted]) as mthOrder FROM   tbl_test_chart_3 WHERE  Year(tbl_test_chart_3.dateSubmitted) = @year";
+
+
+               
+           //     "WHERE  (SOH.SalesOrderNumber = @SalesOrderNumber)";
+
+            SqlConnection connection = new
+                SqlConnection("Data Source=DESKTOP-SPS7F6P; " +
+                              "Initial Catalog=stakeholder; " +
+                              "Integrated Security=SSPI");
+
+            SqlCommand command =
+                new SqlCommand(sqlSalesOrder, connection);
+
+            command.Parameters.Add(
+                new SqlParameter("year",
+                filterYear));
+
+            SqlDataAdapter salesOrderAdapter = new
+                SqlDataAdapter(command);
+
+            salesOrderAdapter.Fill(dsSalesOrder, "DataSet1");
+        }
+    }
+}
